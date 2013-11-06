6c33fba8f6f867c98deacd4643712d2eb746000e
adrigm <adrigm.admin@gmail.com>
2013-11-06 18:33:14 +0000

    Se crea el archivo Core
    Archivo de inclusión único para el módulo Core

--- include/GDE/Core.hpp
+++ include/GDE/Core.hpp
@@ -2,6 +2,9 @@
 #define GDE_CORE_HPP
 
 #include <GDE/Config.hpp>
+#include <GDE/Core/CoreTypes.hpp>
+#include <GDE/Core/StringsUtil.hpp>
+#include <GDE/Core/ConfigReader.hpp>
 #include <GDE/Core/App.hpp>
 
 #endif // GDE_CORE_HPP
