b3b6ccc827f630055713b33674ad7c4fcd2aa59d
adrigm <adrigm.admin@gmail.com>
2013-11-06 18:34:32 +0000

    Se prueba el funcionamiento de ConfigReader

--- src/Test/main.cpp
+++ src/Test/main.cpp
@@ -1,7 +1,13 @@
-#include <GDE/Core/App.hpp>
+#include <GDE/Core.hpp>
+#include <iostream> // Quitar
 
 int main(int argc, char** argv)
 {
+	GDE::ConfigReader conf;
+	conf.loadFromFile("window.cfg");
+
+	std::cout << conf.GetUint32("window", "width", 0);
+
 	GDE::App anApp;
 	
 	anApp.run();
