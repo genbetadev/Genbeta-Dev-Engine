36fd1db3bfe0ee40879727a13250fc5c13846cf0
Ricardo Amores <ricardo.amores@socialpoint.es>
2013-12-02 11:52:46 +0100

    Resources are now correctly copied to the build folder

--- src/Test/main.cpp
+++ src/Test/main.cpp
@@ -5,15 +5,15 @@ int main(int argc, char** argv)
 {
 	// Creamos un codigo de salida
 	int anExitCode = 0;
-//	
-//	// Creamos una instancia de la aplicación
-//	GDE::App* anApp = GDE::App::instance();
-//	
-//	// Creamos una Scene y la establecemos como inicial
-//	anApp->setFirstScene(new SceneMain("Main"));
-//	
-//	// Ejecutamos la aplicación
-//	anExitCode = anApp->run();
+	
+	// Creamos una instancia de la aplicación
+	GDE::Core::App* anApp = GDE::Core::App::instance();
+	
+	// Creamos una Scene y la establecemos como inicial
+	anApp->setFirstScene(new SceneMain("Main"));
+	
+	// Ejecutamos la aplicación
+	anExitCode = anApp->run();
 	
 	// Devolvemos el código de salida de la aplicación
 	return anExitCode;
